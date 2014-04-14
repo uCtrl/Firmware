ccad099239dfe7dfe4a22a20744ce4b5493805a9
Bobby Lavoie <bobby.lavoie@gmail.com>
2014-04-13 23:26:32 -0400

    Merge...

--- Src/Controller/UController.cpp
+++ Src/Controller/UController.cpp
@@ -17,21 +17,11 @@ UController::~UController()
 
 void UController::start()
 {
-<<<<<<< HEAD
-	//char c = '!';
-	while(true)
+	for(;;)
 	{
 		Thread::wait(3000);
 		m_activityLed = false;
 		Thread::wait(200);
 		m_activityLed = true;
-		//c = m_pcSerial.getc();
-		//m_pcSerial.putc(c);
-=======
-	for(;;)
-	{
-		Thread::wait(5000);
-		m_activityLed = !m_activityLed;
->>>>>>> f162662eaa6326c9bab8e4250162f27e6d4cd9fc
 	}
 }
--- Src/main.cpp
+++ Src/main.cpp
@@ -9,15 +9,11 @@
 #include "rtos.h"
 #include "cfg.h"
 #include "UController.h"
-<<<<<<< HEAD
+
 #include "UComDriverIn.h"
 #include "UComDriverOut.h"
 #include "UMsgHandler.h"
-=======
-#include "UComDriver.h"
-#include "UMessageHandler.h"
 #include "UTaskHandler.h"
->>>>>>> f162662eaa6326c9bab8e4250162f27e6d4cd9fc
 
 #ifdef DEBUG_PRINT
 extern Semaphore semMailUTaskHandler;
@@ -29,22 +25,12 @@ extern Mail<UTaskRequest, MAIL_LEN_UTASKHANDLER>mailUTaskHandler;
 #include "UActuatorHandler.h"
 */
 DigitalOut led(LED_RED);
-<<<<<<< HEAD
 
 void startControllerThread(void const *args) {
-=======
-DigitalOut ledg(LED_GREEN);
-
-
-
-void controllerThread(void const *args)
-{
->>>>>>> f162662eaa6326c9bab8e4250162f27e6d4cd9fc
 	UController uController;
 	uController.start();
 }
 
-<<<<<<< HEAD
 void startComDriverInThread(void const *args) {
 	UComDriverIn comDriverIn;
 	comDriverIn.start();
@@ -60,29 +46,12 @@ void startMsgHandlerThread(void const *args) {
 	msgHandler.start();
 }
 
-int main (void) {
-	Thread controllerThread(startControllerThread);
-    Thread comDriverInThread(startComDriverInThread);
-    Thread comDriverOutThread(startComDriverOutThread);
-    Thread msgHandlerThread(startMsgHandlerThread);
-
-    led = true;
-    while (true)
-    {
-    	//led = !led;
-        Thread::wait(5000);
-=======
-void comDriverThread(void const *args)
-{
-	UComDriver uComDriver;
-	uComDriver.start();
-}
-
-void taskHandlerThread(void const *args)
+void startTaskHandlerThread(void const *args)
 {
 	UTaskHandler uTaskHandler;
 	uTaskHandler.start();
 }
+
 /*
 void sensorHandlerThread(void const *args) {
 	USensorHandler uSensorHandler;
@@ -100,54 +69,57 @@ void messageHandlerThread(void const *args) {
 }
 */
 
-int main (void)
-{
-	led = true;
-	ledg = true;
-    Thread ctrlThread(controllerThread,NULL,CONTROLLER_PRIORITY,CONTROLLER_STACK_SIZE);
-    //Thread comThread(comDriverThread,NULL,COM_DRIVER_PRIORITY,COM_DRIVER_STACK_SIZE);
-
-    Thread taskThread(taskHandlerThread,NULL,TASK_HANDLER_PRIORITY,TASK_HANDLER_STACK_SIZE);
-
+int main (void) {
+	Thread controllerThread(startControllerThread,NULL,CONTROLLER_PRIORITY,CONTROLLER_STACK_SIZE);
+    Thread comDriverInThread(startComDriverInThread);
+    Thread comDriverOutThread(startComDriverOutThread);
+    Thread msgHandlerThread(startMsgHandlerThread);
+    Thread taskThread(startTaskHandlerThread,NULL,TASK_HANDLER_PRIORITY,TASK_HANDLER_STACK_SIZE);
     /*
-    Thread sensorThread(sensorHandlerThread,NULL,SENSOR_HANDLER_PRIORITY,SENSOR_HANDLER_STACK_SIZE);
-    Thread actuatorThread(actuatorHandlerThread,NULL,ACTUATOR_HANDLER_PRIORITY,ACTUATOR_HANDLER_STACK_SIZE);
-    Thread messageThread(messageHandlerThread,NULL,MESSAGE_HANDLER_PRIORITY,MESSAGE_HANDLER_STACK_SIZE);
-    */
-    //    osEvent evt;
-    //Thread::wait(5000);
-    int i = 0;
-    Thread::wait(1000);
+        Thread sensorThread(sensorHandlerThread,NULL,SENSOR_HANDLER_PRIORITY,SENSOR_HANDLER_STACK_SIZE);
+        Thread actuatorThread(actuatorHandlerThread,NULL,ACTUATOR_HANDLER_PRIORITY,ACTUATOR_HANDLER_STACK_SIZE);
+        Thread messageThread(messageHandlerThread,NULL,MESSAGE_HANDLER_PRIORITY,MESSAGE_HANDLER_STACK_SIZE);
+	*/
+
+    led = true;
     for(;;)
     {
-    	led = false;
-    	Thread::wait(500);
-    	led = true;
+    	//led = !led;
+        Thread::wait(5000);
 
-    	//for testing purpose
-		#ifdef DEBUG_PRINT
-    		semMailUTaskHandler.wait();
-    		UTaskRequest *mail = mailUTaskHandler.alloc();
-			if(mail != NULL)
-			{
-				i++;
-				printf("%d",i);
-				mail->taskCfg.taskCfgType = UACTION;
-				mail->taskCfg.actionCfg.actuatorId = 123;
-				printf("Send actuatorId:%lu\n\r", mail->taskCfg.actionCfg.actuatorId);
-				mailUTaskHandler.put(mail);
-				printf("Sent");
-			}
-			else
-			{
-				//error, should never happen with semaphore
-				ledg = false;
-				Thread::wait(500);
-				ledg = true;
-			}
-		#endif
-		printf("wait");
-		Thread::wait(2000);
->>>>>>> f162662eaa6326c9bab8e4250162f27e6d4cd9fc
     }
+
+//    int i = 0;
+//	Thread::wait(1000);
+//	for(;;)
+//	{
+//		led = false;
+//		Thread::wait(500);
+//		led = true;
+//
+//		//for testing purpose
+//		#ifdef DEBUG_PRINT
+//			semMailUTaskHandler.wait();
+//			UTaskRequest *mail = mailUTaskHandler.alloc();
+//			if(mail != NULL)
+//			{
+//				i++;
+//				printf("%d",i);
+//				mail->taskCfg.taskCfgType = UACTION;
+//				mail->taskCfg.actionCfg.actuatorId = 123;
+//				printf("Send actuatorId:%lu\n\r", mail->taskCfg.actionCfg.actuatorId);
+//				mailUTaskHandler.put(mail);
+//				printf("Sent");
+//			}
+//			else
+//			{
+//				//error, should never happen with semaphore
+//				ledg = false;
+//				Thread::wait(500);
+//				ledg = true;
+//			}
+//		#endif
+//		printf("wait");
+//		Thread::wait(2000);
+//	}
 }
